fb07444ceb64f00acd074bc72d3f60b1130a950b
unknown <PS22SBADER@efcts.us>
2021-11-15 12:41:01 -0500

    new: half-functionality menu screen

---
+++ Assets/Scripts/MenuButtonBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButtonBehavior : MonoBehaviour
+{
+    public Text Self;
+    public Text Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Text.GetComponent<menuStartup>().Selection == Self)
+        {
+            Self.color = Color.yellow;
+            Self.fontStyle = FontStyle.BoldAndItalic;
+            
+        }
+        else
+        {
+            Self.color = Color.white;
+            Self.fontStyle = FontStyle.Normal;
+        }
+    }
+}
--- Assets/Scripts/menuStartup.cs
+++ Assets/Scripts/menuStartup.cs
@@ -8,18 +8,104 @@ using UnityEngine.SceneManagement;
 public class menuStartup : MonoBehaviour
 {
     public Text myText;
-    private bool startKeyCheck = false;
+    public Text Play;
+    public Text Load;
+    public Text Credits;
+    public Text Exit;
+    public Text Achievements;
+    public Text Selection;
+    List<Text> Buttons = new List<Text>();
+    int SelectionIndex;
+    bool isLoaded = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        Selection = Play;
+        Buttons.Add(Play);
+        Buttons.Add(Load);
+        Buttons.Add(Credits);
+        Buttons.Add(Exit);
+        Buttons.Add(Achievements);
+        SelectionIndex = Buttons.IndexOf(Selection);
+        Play.gameObject.SetActive(false);
+        Load.gameObject.SetActive(false);
+        Credits.gameObject.SetActive(false);
+        Exit.gameObject.SetActive(false);
+        Achievements.gameObject.SetActive(false);
         StartCoroutine(IntroSequencePT1());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        startKeyCheckfunc();
+        if (isLoaded == true)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (SelectionIndex - 1 == -1)
+                {
+                    SelectionIndex = Buttons.Count - 1;
+                }
+                else
+                {
+                    SelectionIndex -= 1;
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                if (SelectionIndex + 1 > Buttons.Count - 1)
+                {
+                    SelectionIndex = 0;
+                }
+                else
+                {
+                    SelectionIndex += 1;
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.Return))
+            {
+                if (Selection == Buttons[0]) //play
+                {
+                    SceneManager.UnloadSceneAsync("TitleScreen");
+                    SceneManager.LoadSceneAsync("Main");
+                }
+                else if (Selection == Buttons[1]) //load
+                {
+
+                }
+                else if (Selection == Buttons[2]) //credits
+                {
+
+                }
+                else if (Selection == Buttons[3]) //exit
+                {
+                    Application.Quit();
+                }
+                else if (Selection == Buttons[4]) //achievements
+                {
+
+                }
+            }
+
+            switch (SelectionIndex)
+            {
+                case 0:
+                    Selection = Buttons[0];
+                    break;
+                case 1:
+                    Selection = Buttons[1];
+                    break;
+                case 2:
+                    Selection = Buttons[2];
+                    break;
+                case 3:
+                    Selection = Buttons[3];
+                    break;
+                case 4:
+                    Selection = Buttons[4];
+                    break;
+            }
+        }
     }
 
     IEnumerator IntroSequencePT1()
@@ -31,59 +117,11 @@ public class menuStartup : MonoBehaviour
         yield return new WaitForSeconds(2);
         myText.text += ".";
         yield return new WaitForSeconds(2);
-        myText.text += "\nPERFORMING STANDARD CHECKS...";
-        yield return new WaitForSeconds(2);
-        myText.text += ".";
-        yield return new WaitForSeconds(2);
-        myText.text += ".";
-        yield return new WaitForSeconds(2);
-        myText.text += ".";
-        yield return new WaitForSeconds(2);
-        myText.text += "\nDOUBLE CHECKING...";
-        yield return new WaitForSeconds(1);
-        myText.text += ".";
-        yield return new WaitForSeconds(1);
-        myText.text += ".";
-        yield return new WaitForSeconds(1);
-        myText.text += ".";
-        yield return new WaitForSeconds(1);
-        myText.text += "\nMANAGING PACKAGES...";
-        yield return new WaitForSeconds(3);
-        myText.text += ".";
-        yield return new WaitForSeconds(2);
-        myText.text += ".";
-        yield return new WaitForSeconds(1);
-        myText.text += ".";
-        yield return new WaitForSeconds(1);
-        myText.text += "\nLOGON:  ";
-        myText.text += ".";
-        yield return new WaitForSeconds(1);
-        myText.text += ".";
-        yield return new WaitForSeconds(1);
-        myText.text += ".";
-        yield return new WaitForSeconds(1);
-        myText.text += ".";
-        yield return new WaitForSeconds(1);
-        myText.text += "\nCONFIRMED.";
-        yield return new WaitForSeconds(2);
-        myText.text += "\n\n____________________\n- NOSTROMO SYSTEMS -\n____________________\n\n     Welcome...\n                       ...PRESS ANY KEY TO ENTER...";
-        startKeyCheck = true;
-    }
-
-    private void startKeyCheckfunc()
-    {
-        if(startKeyCheck == false)
-        {
-            return;
-        }
-        else
-        {
-            if (Input.anyKey)
-            {
-
-                SceneManager.LoadScene("Main");
-                SceneManager.UnloadSceneAsync("TitleScreen");
-            }
-        }
+        Play.gameObject.SetActive(true);
+        Load.gameObject.SetActive(true);
+        Credits.gameObject.SetActive(true);
+        Exit.gameObject.SetActive(true);
+        Achievements.gameObject.SetActive(true);
+        isLoaded = true;
     }
 }
