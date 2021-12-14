05b00cf37787c59aad7c847ef519c82644b4e719
unknown <PS22SBADER@efcts.us>
2021-12-14 13:38:15 -0500

    new: MASSIVE start area revamp, new square usage

---
+++ Assets/exitAreaScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitAreaScript : MonoBehaviour
+{
+    public GameObject self;
+    public GameObject drone;
+
+    void Update()
+    {
+        if (self.GetComponent<Collider2D>().IsTouching(drone.GetComponent<Collider2D>()))
+        {
+            //close "Main"
+            //load "NavigationRoomMPTerminal.SelectedScene"
+            drone.GetComponent<DroneStuff.DroneMovement>().DoNotAllowMovement = true;
+        }
+    }
+}
